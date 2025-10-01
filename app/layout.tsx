8252d09c7b3163523e5972b28ab5e0af6a9286e1
Aparna1567 <aparnaapar99@gmail.com>
2025-10-01 05:50:00 +0530

    implement header

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Signalist",
-  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
+  description: "Track real-time stock prices, get personalized alerts and explore company insights.",
 };
 
 export default function RootLayout({
